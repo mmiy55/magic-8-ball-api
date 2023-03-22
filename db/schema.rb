0450cb61dc0bf28133b4016019d89311636cb3a1
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-22 22:43:52 +0900

    add models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_22_131223) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_133238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "content"
+    t.string "type"
+    t.bigint "user_id", null: false
+    t.bigint "ball_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ball_id"], name: "index_answers_on_ball_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "balls", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_balls_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_22_131223) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "balls"
+  add_foreign_key "answers", "users"
+  add_foreign_key "balls", "users"
 end
