a86e9b1247675a011ef520823399f682938b8ee1
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-22 22:38:51 +0900

    add validation to answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :ball
+
+  validates :content, inclusion: { in: %w(positive negative vague),
+    message: "%{value} is not a valid size" }
 end
