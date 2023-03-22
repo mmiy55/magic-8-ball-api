874a781dfc009e04e9dc7f67e2d7cabf6b844a44
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-22 22:53:17 +0900

    create balls route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :balls, only: [ :index ]
+    end
+  end
 end
