36a9c03bf3448c9bf2d055e0b373d28d1d337bab
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-23 13:33:38 +0900

    add auth exeption for #shake

--- app/controllers/api/v1/balls_controller.rb
+++ app/controllers/api/v1/balls_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BallsController < Api::V1::BaseController
-  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  acts_as_token_authentication_handler_for User, except: [ :index, :show, :shake ]
 
   before_action :set_ball, only: [ :show, :update, :destroy, :shake ]
   def index
