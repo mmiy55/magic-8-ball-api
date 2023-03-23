7ee9da8fa82f61c2fe075fd5d0023f46e751541a
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-23 09:46:47 +0900

    add token to user

--- app/controllers/api/v1/balls_controller.rb
+++ app/controllers/api/v1/balls_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BallsController < Api::V1::BaseController
-  before_action :set_ball, only: [ :show ]
+  before_action :set_ball, only: [ :show, :update ]
   def index
     @balls = policy_scope(Ball)
   end
@@ -8,10 +8,28 @@ class Api::V1::BallsController < Api::V1::BaseController
   def show
   end
 
+  def update
+    if @ball.update(ball_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
   private
 
   def set_ball
     @ball = Ball.find(params[:id])
     authorize @ball  # For Pundit
   end
+
+  def ball_params
+    params.require(:ball).permit(:name, :comment)
+  end
+
+  def render_error
+    render json: { errors: @ball.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :balls
 
+  acts_as_token_authenticatable
+
 end
